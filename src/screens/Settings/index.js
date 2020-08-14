49add4a964c5b9586ec00d47c314b4686b2b2b89
Carlos Daniel <danielbarboza942@gmail.com>
2020-08-14 13:48:33 -0300

    ⚙️  page setted

--- src/screens/Settings/index.js
+++ src/screens/Settings/index.js
@@ -1,8 +1,173 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { AntDesign } from '@expo/vector-icons'; 
+
+import {
+    Wrapper,
+    Container,
+    Header,
+    HelpView,
+    HelpLabel,
+    Img,
+    UserNick,
+    UserName,
+    ProfileButton,
+    ProfileButtonLabel,
+    SettingsComponentContainer,
+    SettingsComponentTitle,
+    SettingsComponent,
+    SettingsComponentInsideText,
+    SettingsComponentInsideTitle,
+    SettingsComponentLogoffTitle,
+    BottomSpace,   
+} from './styles';
+
+import avatar from '../../images/avatar.png';
 
 function Settings() {
-    return <Text>Settings</Text>
+    return (
+        <Wrapper>
+            <Container>
+                <Header>
+                    <HelpView>
+                        <HelpLabel>Ajuda</HelpLabel>
+                    </HelpView>
+
+                    <Img source={avatar} />
+
+                    <UserNick>@carlosdnba</UserNick>
+                    <UserName>Carlos Daniel do N. Barboza</UserName>
+
+                    <ProfileButton>
+                        <ProfileButtonLabel>Ver meu perfil <AntDesign name="caretright" size={14} color="#0db060" /> </ProfileButtonLabel>
+                    </ProfileButton>
+                </Header>
+
+                <SettingsComponentContainer>
+                    <SettingsComponentTitle>Minha conta</SettingsComponentTitle>
+
+                    <SettingsComponent>
+                        <SettingsComponentInsideTitle>Meu número</SettingsComponentInsideTitle>
+                        <SettingsComponentInsideText>celular</SettingsComponentInsideText>
+                    </SettingsComponent>
+
+                    <SettingsComponent>
+                        <SettingsComponentInsideTitle>Meu E-mail</SettingsComponentInsideTitle>
+                        <SettingsComponentInsideText>d************2@gmail.com</SettingsComponentInsideText>
+                    </SettingsComponent>
+                    
+                    <SettingsComponent>
+                        <SettingsComponentInsideTitle>Dados pessoais</SettingsComponentInsideTitle>
+                        <SettingsComponentInsideText>Nome, CPF e data de nascimento</SettingsComponentInsideText>
+                    </SettingsComponent>
+                    
+                    <SettingsComponent>
+                        <SettingsComponentInsideTitle>Conta Bancária</SettingsComponentInsideTitle>
+                    </SettingsComponent>
+                    
+                    <SettingsComponent>
+                        <SettingsComponentInsideTitle>Taxas e limites</SettingsComponentInsideTitle>
+                    </SettingsComponent>
+                    
+                    <SettingsComponent>
+                        <SettingsComponentInsideTitle>Meus endereços</SettingsComponentInsideTitle>
+                    </SettingsComponent>
+                    
+                    <SettingsComponent>
+                        <SettingsComponentInsideTitle>Meus favoritos</SettingsComponentInsideTitle>
+                    </SettingsComponent>
+                    
+                    <SettingsComponent>
+                        <SettingsComponentInsideTitle>Validar identidade</SettingsComponentInsideTitle>
+                        <SettingsComponentInsideText>Não verificado</SettingsComponentInsideText>
+                    </SettingsComponent>
+                    
+                    <SettingsComponent>
+                        <SettingsComponentInsideTitle>Atualizar cadastro</SettingsComponentInsideTitle>
+                    </SettingsComponent>
+                </SettingsComponentContainer>
+
+                <SettingsComponentContainer>
+                    <SettingsComponentTitle>Promoções</SettingsComponentTitle>
+                    
+                    <SettingsComponent>
+                        <SettingsComponentInsideTitle>Usar código promocional</SettingsComponentInsideTitle>
+                    </SettingsComponent>
+                </SettingsComponentContainer>
+
+                <SettingsComponentContainer>
+                    <SettingsComponentTitle>Minhas assinaturas</SettingsComponentTitle>
+                    
+                    <SettingsComponent>
+                        <SettingsComponentInsideTitle>Ver minhas assinaturas</SettingsComponentInsideTitle>
+                    </SettingsComponent>
+                </SettingsComponentContainer>
+
+                <SettingsComponentContainer>
+                    <SettingsComponentTitle>Para o meu negócio</SettingsComponentTitle>
+                    
+                    <SettingsComponent>
+                        <SettingsComponentInsideTitle>PicPay PRO</SettingsComponentInsideTitle>
+                    </SettingsComponent>
+
+                    <SettingsComponent>
+                        <SettingsComponentInsideTitle>Para estabelecimentos</SettingsComponentInsideTitle>
+                    </SettingsComponent>
+                    
+                    <SettingsComponent>
+                        <SettingsComponentInsideTitle>Venda por assinaturas</SettingsComponentInsideTitle>
+                    </SettingsComponent>
+                </SettingsComponentContainer>
+
+                <SettingsComponentContainer>
+                    <SettingsComponentTitle>Configurações</SettingsComponentTitle>
+                    
+                    <SettingsComponent>
+                        <SettingsComponentInsideTitle>Usar digital pra pagar</SettingsComponentInsideTitle>
+                    </SettingsComponent>
+                    
+                    <SettingsComponent>
+                        <SettingsComponentInsideTitle>Alterar senha</SettingsComponentInsideTitle>
+                    </SettingsComponent>
+                    
+                    <SettingsComponent>
+                        <SettingsComponentInsideTitle>Privacidade</SettingsComponentInsideTitle>
+                    </SettingsComponent>
+                    
+                    <SettingsComponent>
+                        <SettingsComponentInsideTitle>Notificações</SettingsComponentInsideTitle>
+                    </SettingsComponent>
+                    
+                    <SettingsComponent>
+                        <SettingsComponentInsideTitle>Contas vinculadas</SettingsComponentInsideTitle>
+                    </SettingsComponent>
+                </SettingsComponentContainer>
+
+                <SettingsComponentContainer>
+                    <SettingsComponentTitle>Geral</SettingsComponentTitle>
+                    
+                    <SettingsComponent>
+                        <SettingsComponentInsideTitle>Precisa de ajuda?</SettingsComponentInsideTitle>
+                    </SettingsComponent>
+                    
+                    <SettingsComponent>
+                        <SettingsComponentInsideTitle>Sobre o PicPay</SettingsComponentInsideTitle>
+                    </SettingsComponent>
+                    
+                    <SettingsComponent>
+                        <SettingsComponentInsideTitle>Desativar minha conta</SettingsComponentInsideTitle>
+                    </SettingsComponent>
+                </SettingsComponentContainer>
+
+                <SettingsComponentContainer>
+                    <SettingsComponent>
+                        <SettingsComponentLogoffTitle>Sair</SettingsComponentLogoffTitle>
+                    </SettingsComponent>
+                </SettingsComponentContainer>
+
+                <BottomSpace />
+            </Container>
+        </Wrapper>    
+    )
 }
 
 export default Settings;
---
+++ src/screens/Settings/styles.js
@@ -0,0 +1,99 @@
+import styled from 'styled-components/native';
+
+export const Wrapper = styled.View`
+    flex: 1;
+    background: #1c1c1e
+`;
+export const Container = styled.ScrollView``;
+
+export const Header = styled.View`
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const HelpView = styled.TouchableOpacity`
+    margin-top: 20px;
+    margin-right: 20px;
+    flex-direction: row;
+    align-self: flex-end;
+`;
+
+export const HelpLabel = styled.Text`
+    color: #0db060;
+    font-size: 16px;
+    font-weight: bold;
+`;
+
+export const Img = styled.Image`
+    margin-top: 30px;
+    width: 100px;
+    height: 100px;
+`;
+
+export const UserNick = styled.Text`
+    margin-top: 10px;
+    color: #fff;
+    font-size: 20px;
+    font-weight: bold;
+`;
+
+export const UserName = styled.Text`
+    margin-top: 5px;
+    color: #8e8e93;
+    font-size: 14px;
+    font-weight: 100;
+    
+`;
+
+export const ProfileButton = styled.TouchableOpacity`
+    margin-top: 8px;
+`;
+
+export const ProfileButtonLabel = styled.Text`
+    color: #0db060;
+`;
+
+
+
+export const SettingsComponentContainer = styled.View`
+    margin-top: 15px;
+`;
+
+export const SettingsComponentTitle = styled.Text`
+    margin-left: 15px;
+    margin-bottom: 8px;
+    color: #0db060;
+    font-weight: bold;
+`;
+
+export const SettingsComponent = styled.TouchableOpacity`
+    background: #000;
+    border: 0.2px solid #4f4f4f;
+    height: 70px;
+    padding: 10px;
+
+    flex-direction: column;
+    justify-content: space-evenly;
+`;
+
+export const SettingsComponentInsideTitle = styled.Text`
+    color: #fff;
+    font-size: 14px;
+    font-weight: bold;
+`;
+
+export const SettingsComponentInsideText = styled.Text`
+    color: #8e8e93;
+    font-size: 14px;
+    font-weight: 100;
+`;
+
+export const SettingsComponentLogoffTitle = styled.Text`
+    color: red;
+    font-size: 14px;
+    font-weight: bold;
+`;
+
+export const BottomSpace = styled.View`
+    height: 30px;
+`;
